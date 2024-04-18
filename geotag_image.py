d5b125156fe90d6095a07f2b804aa7aed5bbc84e
GiYoung Mun <56053651+munn129@users.noreply.github.com>
2024-04-19 00:39:04 +0900

    Image를 인스턴스에서 저장하고 있지 않는 방향으로 수정
    기존에 이미지를 인스턴스에서 저장하고 있는 방식이었음.
    하지만 이런 방식은 인스턴스에 너무 많은 메모리를 할당하게 만듦.
    그래서 get_image() 호출 시에 이미지를 읽어서 반환하도록 바꿈.
    GeoTagImage는 이미지의 경로만 저장함.

--- geotag_image.py
+++ geotag_image.py
@@ -1,3 +1,8 @@
+'''
+Image와 GPS, Azimuth 값을 저장하는 클래스
+Image는 경로만 저장하며, get_image() 호출 시 이미지를 읽는다.
+'''
+
 import cv2
 
 class GeoTagImage:
@@ -5,9 +10,7 @@ class GeoTagImage:
         self.latitude = 0
         self.longitude = 0
         self.azimuth = 0
-
-        self.image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        if self.image is None: raise Exception("image is None")
+        self.image_path = image_path
         
         # read gps
         with open(gps_path, 'r') as file:
@@ -23,7 +26,9 @@ class GeoTagImage:
             raise Exception("Wrong GPS or azimuth value")
 
     def get_image(self):
-        return self.image
+        image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
+        if self.image is None: raise Exception("image is None")
+        return image
     
     def get_latitude(self):
         return self.latitude

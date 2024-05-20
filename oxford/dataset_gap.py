1032d3acc99af5ea18d4cd1773597565e6df1121
cvlab_moon <ans92175@gmail.com>
2024-05-20 15:12:21 +0900

    average dataset gap distance : 2.608m, over 10meter 58, 50meter 2 내 생각엔 데이터셋 구성 중 인터폴레이션 하는 부분에서 문제가 있다고 봄. (시퀀스 이미지 + gps인데 50미터 이상 차이날 이유가 없다고 생각)

--- oxford/dataset_gap.py
+++ oxford/dataset_gap.py
@@ -7,15 +7,21 @@ def main():
     with open(gps, 'r') as file:
         for line in file:
             line = line.split()
+            if line[0] == '#': continue
+            # print(line)
             gps_list.append((float(line[1]), float(line[2])))
 
-    print(gps_list)
+    # print(gps_list)
 
     distance = 0
+    cnt = 0
     for i in range(len(gps_list) -1):
-        distance += gps_to_meter(gps_list[i], gps_list[i+1])
+        gap = gps_to_meter(gps_list[i], gps_list[i+1])
+        if gap > 50: cnt += 1
+        distance += gap
 
     print(f'dataset distance : {distance/(len(gps_list) - 1)}')
+    print(cnt)
 
 if __name__ == '__main__':
     main()
